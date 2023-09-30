f5faef8c551f4f2a3e458c2c2217dbdea20548b6
ybsgit <yograj.bharat@gmail.com>
2023-09-30 11:38:08 +0530

    uncommented docker push

--- vars/pushImage1.groovy
+++ vars/pushImage1.groovy
@@ -1,11 +1,11 @@
 def call(String project,String tag,String user,String accountId,String region){
-    // withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
-    // sh """
-    //  docker login -u $USER -p $PASSWORD
-    //  docker image push  ${user}/${project}:${tag}
-    //  docker image push  ${user}/${project}:latest
-    // """
-//}
+    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
+    sh """
+     docker login -u $USER -p $PASSWORD
+     docker image push  ${user}/${project}:${tag}
+     docker image push  ${user}/${project}:latest
+    """
+}
 
 
 withCredentials([usernamePassword(credentialsId: 'aws-cred-01', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
