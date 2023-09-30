e170ad1eb36cb38920693d03a1d49f4db04eacc5
ybsgit <yograj.bharat@gmail.com>
2023-09-30 10:12:55 +0530

    updated pushimage

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,9 +1,13 @@
-def call(String project,String tag,String user){
+def call(String project,String tag,String user,String accountId,String region){
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASSWORD', usernameVariable: 'USER')]) {
     sh """
      docker login -u $USER -p $PASSWORD
-        docker image push  ${user}/${project}:${tag}
-         docker image push  ${user}/${project}:latest
+     docker image push  ${user}/${project}:${tag}
+     docker image push  ${user}/${project}:latest
     """
 }
+sh """
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin  ${accountId}.dkr.ecr. ${region}.amazonaws.com
+ docker image push image tag  ${user}/${project} ${accountId}.dkr.ecr.${region}.amazonaws.com/${project}:${tag}
+"""
 }
