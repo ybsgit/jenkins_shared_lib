f09a3fc4ff4186894bb7a8872394cca6a08829f9
ybsgit <yograj.bharat@gmail.com>
2023-09-30 08:52:39 +0530

    added ecr docker build

--- vars/dockerBuild1.groovy
+++ vars/dockerBuild1.groovy
@@ -1,7 +1,8 @@
-def call(String project,String tag,String user){
+def call(String project,String tag,String user,String accountId.String region){
     sh """
        docker image build -t ${user}/${project} . 
        docker image tag  ${user}/${project}  ${user}/${project}:${tag}
        docker image tag  ${user}/${project}  ${user}/${project}:latest
+       docker image tag  ${user}/${project} ${accountId}.dkr.ecr.${accountId}.amazonaws.com/${project}:${tag}
     """
 }
