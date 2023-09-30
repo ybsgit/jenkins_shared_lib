00a7b98497a67c6f86c75febfe649eb398fa0b52
ybsgit <yograj.bharat@gmail.com>
2023-09-30 10:57:05 +0530

    added aws creds

--- vars/pushImage1.groovy
+++ vars/pushImage1.groovy
@@ -6,8 +6,10 @@ def call(String project,String tag,String user,String accountId,String region){
      docker image push  ${user}/${project}:latest
     """
 }
-withCredentials([<object of type com.cloudbees.jenkins.plugins.awscredentials.AmazonWebServicesCredentialsBinding>]) {
-   sh """
+
+
+withCredentials([usernamePassword(credentialsId: 'aws-cred-01', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+sh """
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin  ${accountId}.dkr.ecr.${region}.amazonaws.com
  docker image push image tag  ${user}/${project} ${accountId}.dkr.ecr.${region}.amazonaws.com/${project}:${tag}
 """
