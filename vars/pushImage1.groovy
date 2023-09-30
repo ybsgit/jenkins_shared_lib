481cdc254810d791ad2071a173805a7b70bf19bd
ybsgit <yograj.bharat@gmail.com>
2023-09-30 10:46:26 +0530

    uuncommented ecr code

--- vars/pushImage1.groovy
+++ vars/pushImage1.groovy
@@ -6,8 +6,8 @@ def call(String project,String tag,String user,String accountId,String region){
      docker image push  ${user}/${project}:latest
     """
 }
-// sh """
-// aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin  ${accountId}.dkr.ecr.${region}.amazonaws.com
-//  docker image push image tag  ${user}/${project} ${accountId}.dkr.ecr.${region}.amazonaws.com/${project}:${tag}
-// """
+sh """
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin  ${accountId}.dkr.ecr.${region}.amazonaws.com
+ docker image push image tag  ${user}/${project} ${accountId}.dkr.ecr.${region}.amazonaws.com/${project}:${tag}
+"""
 }
