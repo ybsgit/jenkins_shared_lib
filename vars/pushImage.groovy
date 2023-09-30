9c2f5212196310f3c7d288680b639b1e74be454c
ybsgit <yograj.bharat@gmail.com>
2023-09-30 10:30:48 +0530

    fixed symtax error in pushimage

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -7,7 +7,7 @@ def call(String project,String tag,String user,String accountId,String region){
     """
 }
 sh """
-aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin  ${accountId}.dkr.ecr. ${region}.amazonaws.com
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin  ${accountId}.dkr.ecr.${region}.amazonaws.com
  docker image push image tag  ${user}/${project} ${accountId}.dkr.ecr.${region}.amazonaws.com/${project}:${tag}
 """
 }
